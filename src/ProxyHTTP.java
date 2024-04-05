4bb53b9d6cc50817a74ac0c3b532c0d6b0f3b1ad
unknown <ywanes@gmail.com>
2024-04-05 16:00:05 -0300

    v0.0.0.1.0

--- src/ProxyHTTP.java
+++ src/ProxyHTTP.java
@@ -35,9 +35,71 @@ public class ProxyHTTP implements Proxy{
     this.user=user;
     this.passwd=passwd;
   }
+  static Socket createSocket(String host, int port, int timeout) throws JSchException{
+    Socket socket=null;
+    if(timeout==0){
+      try{
+        socket=new Socket(host, port);
+        return socket;
+      }
+      catch(Exception e){
+          ALoadClass.DebugPrintException("ex_162");
+        String message=e.toString();
+        if(e instanceof Throwable)
+          throw new JSchException(message, (Throwable)e);
+        throw new JSchException(message);
+      }
+    }
+    final String _host=host;
+    final int _port=port;
+    final Socket[] sockp=new Socket[1];
+    final Exception[] ee=new Exception[1];
+    String message="";
+    Thread tmp=new Thread(new Runnable(){
+        public void run(){
+          sockp[0]=null;
+          try{
+            sockp[0]=new Socket(_host, _port);
+          }
+          catch(Exception e){
+              ALoadClass.DebugPrintException("ex_163");
+            ee[0]=e;
+            if(sockp[0]!=null && sockp[0].isConnected()){
+              try{
+                sockp[0].close();
+              }
+              catch(Exception eee){}
+            }
+            sockp[0]=null;
+          }
+        }
+      });
+    tmp.setName("Opening Socket "+host);
+    tmp.start();
+    try{ 
+      tmp.join(timeout);
+      message="timeout: ";
+    }
+    catch(java.lang.InterruptedException eee){
+    }
+    if(sockp[0]!=null && sockp[0].isConnected()){
+      socket=sockp[0];
+    }
+    else{
+      message+="socket is not established";
+      if(ee[0]!=null){
+        message=ee[0].toString();
+      }
+      tmp.interrupt();
+      tmp=null;
+      throw new JSchException(message, ee[0]);
+    }
+    return socket;
+  } 
+  
   public void connect(SocketFactory socket_factory, String host, int port, int timeout) throws JSchException{
     try{
-        socket=Util.createSocket(proxy_host, proxy_port, timeout);    
+        socket=createSocket(proxy_host, proxy_port, timeout);    
         in=socket.getInputStream();
         out=socket.getOutputStream();
       if(timeout>0){
--- src/Util.java
+++ src/Util.java
@@ -5,13 +5,6 @@ import java.io.IOException;
 
 class Util{
   private static final byte[] b64 =Util.str2byte("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=");
-  private static byte val(byte foo){
-    if(foo == '=') return 0;
-    for(int j=0; j<b64.length; j++){
-      if(foo==b64[j]) return (byte)j;
-    }
-    return 0;
-  }
 
   static String[] split(String foo, String split){
     if(foo==null)
@@ -36,68 +29,6 @@ class Util{
     }
     return result;
   }
-
-  static Socket createSocket(String host, int port, int timeout) throws JSchException{
-    Socket socket=null;
-    if(timeout==0){
-      try{
-        socket=new Socket(host, port);
-        return socket;
-      }
-      catch(Exception e){
-          ALoadClass.DebugPrintException("ex_162");
-        String message=e.toString();
-        if(e instanceof Throwable)
-          throw new JSchException(message, (Throwable)e);
-        throw new JSchException(message);
-      }
-    }
-    final String _host=host;
-    final int _port=port;
-    final Socket[] sockp=new Socket[1];
-    final Exception[] ee=new Exception[1];
-    String message="";
-    Thread tmp=new Thread(new Runnable(){
-        public void run(){
-          sockp[0]=null;
-          try{
-            sockp[0]=new Socket(_host, _port);
-          }
-          catch(Exception e){
-              ALoadClass.DebugPrintException("ex_163");
-            ee[0]=e;
-            if(sockp[0]!=null && sockp[0].isConnected()){
-              try{
-                sockp[0].close();
-              }
-              catch(Exception eee){}
-            }
-            sockp[0]=null;
-          }
-        }
-      });
-    tmp.setName("Opening Socket "+host);
-    tmp.start();
-    try{ 
-      tmp.join(timeout);
-      message="timeout: ";
-    }
-    catch(java.lang.InterruptedException eee){
-    }
-    if(sockp[0]!=null && sockp[0].isConnected()){
-      socket=sockp[0];
-    }
-    else{
-      message+="socket is not established";
-      if(ee[0]!=null){
-        message=ee[0].toString();
-      }
-      tmp.interrupt();
-      tmp=null;
-      throw new JSchException(message, ee[0]);
-    }
-    return socket;
-  } 
 
   static byte[] str2byte(String str, String encoding){
     if(str==null) 
