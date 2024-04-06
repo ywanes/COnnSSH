e1b4e8528ebf79cb7019c2972c396c132e436ec4
unknown <ywanes@gmail.com>
2024-04-05 21:13:40 -0300

    v0.0.0.1.0

--- src/JSchCustom.java
+++ src/JSchCustom.java
@@ -9,20 +9,19 @@ class Ssh {
     public static void custom(String arg0, String password, int port) {
         Channel channel = null;
         try {
-            JSch jsch = new JSch();
             if ( !arg0.contains("@") ) {
                 System.err.println("usage: y ssh user,pass@remotehost");
                 System.exit(-1);
             }
             String user = arg0.split("@")[0];
             String host = arg0.split("@")[1];
-            Session session = jsch.getSession(user, host, port);
+            Session session = new JSch().getSession(user, host, port);
             session.setPassword(password);
             session.connect(30000);
             channel = session.openChannel("shell");
             channel.setInputStream(System.in);
             channel.setOutputStream(System.out);
-            channel.connect(3 * 1000);
+            channel.connect(3000);
         } catch (Exception e) {
             System.out.println(e);
         }
